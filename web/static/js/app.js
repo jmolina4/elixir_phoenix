58a2737edf9865f44985404941a05b72c1d62c14
jmolina <javi.ms10@gmail.com>
2016-11-17 01:36:18 +0100

    wip

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -11,7 +11,11 @@
 //
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
-import "phoenix_html"
+
+//= require jquery
+//= require jquery_ujs
+//= require admin-lte/bootstrap/js/bootstrap
+//= require admin-lte/dist/js/app.js
 
 // Import local files
 //
