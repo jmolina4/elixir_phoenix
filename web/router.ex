5669b40e5c2502ab9594ad4a344c546329af29ce
Javier Molina Sánchez <jmolina@digitalorigin.com>
2017-02-07 08:30:03 +0100

    AdminLTE set

--- web/router.ex
+++ web/router.ex
@@ -13,20 +13,28 @@ defmodule Dogfamily.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :back_office_layout do
+    plug :put_layout, { Dogfamily.LayoutView, :back_office }
+  end
+
+  scope "/back_office", Dogfamily do
+    pipe_through [:browser, :back_office_layout]
+
+    get "/", BackOfficeController, :index
+    get "/back_office/login", SessionController, :new
+    post "/back_office/login",  SessionController, :create
+
+    resources "/back_office/users", UserController
+    resources "/back_office/roles", RoleController
+    resources "/back_office/residences", ResidenceController do
+      resources "/dogs", DogController
+    end
+  end
+
   scope "/", Dogfamily do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
-    get "/back_office", BackOfficeController, :index do
-      get "/back_office/login", SessionController, :new
-      post "/back_office/login",  SessionController, :create
-
-      resources "/back_office/users", UserController
-      resources "/back_office/roles", RoleController
-      resources "/back_office/residences", ResidenceController do
-        resources "/dogs", DogController
-      end
-    end
   end
 
   # Other scopes may use custom stacks.
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -12,14 +12,12 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 
-//= require jquery
-//= require jquery_ujs
-//= require admin-lte/bootstrap/js/bootstrap
-//= require admin-lte/dist/js/app.js
-
 // Import local files
 //
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+require('jquery')
+require('admin-lte')
