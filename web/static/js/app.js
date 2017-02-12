2412f58c2e383913639ab5e29f0f101a1d700c3d
Javier Molina Sánchez <javi.ms10@gmail.com>
2017-02-12 20:33:43 +0100

    Feature/change back office style (#8)
    * wip
    
    * Using bower
    
    * AdminLTE set

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -11,7 +11,6 @@
 //
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
-import "phoenix_html"
 
 // Import local files
 //
@@ -19,3 +18,6 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+require('jquery')
+require('admin-lte')
