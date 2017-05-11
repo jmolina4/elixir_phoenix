3be05e721a16c1924f6a993ed6130282534df36b
jmolina <javi.ms10@gmail.com>
2017-05-12 00:50:41 +0200

    Adding seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -5,7 +5,18 @@
 # Inside the script, you can read and write to any of your
 # repositories directly:
 #
-#     Dogfamily.Repo.insert!(%Dogfamily.SomeModel{})
+alias Dogfamily.Repo
+alias Dogfamily.User
+alias Dogfamily.Role
+alias Dogfamily.Registration
+
+Repo.delete_all(Role)
+Repo.delete_all(User)
+
+Repo.insert!(%Role{:name => "Admin", :admin => true})
+
+changeset = User.changeset(%User{}, %{:email => "javi.ms10@gmail.com", :password => "123456", :role_id => 1})
+Registration.create(changeset, Repo)
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
