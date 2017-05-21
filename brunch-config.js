b4d53a0cbf4116b5752d5f334aecf62fbbba22a1
jmolina91 <javi.ms10@gmail.com>
2017-05-21 14:38:34 +0200

    Reorder app

--- brunch-config.js
+++ brunch-config.js
@@ -14,7 +14,11 @@ exports.config = {
 
     },
     stylesheets: {
-      joinTo: "css/app.css",
+      joinTo: {
+        "css/app.css": /^(web\/static\/css\/app)/,
+        "css/back_office.css": /^(web\/static\/css\/back_office)/
+      },
+
       order: {
         after: ["web/static/css/app.css"] // concat app.css last
       }
